66ef7ae992a79b8a7ad5c26b044f20a562e6fff4
Galerakis Nikos <nikolisgal@gmail.com>
2013-06-12 15:14:54 +0200

    SecondComit

--- LastTestProject/src/com/resources/mainMethod/MainClass.java
+++ LastTestProject/src/com/resources/mainMethod/MainClass.java
@@ -4,13 +4,6 @@ public class MainClass {
 
 	public static void main(String[] args){
 	System.out.println("This a asample output");
-	System.out.println("Kai kati akoma") ; 
-	System.out.println("This a asample output");
-	System.out.println("Kai kati akoma") ; 
-	System.out.println("This a asample output");
-	System.out.println("Kai kati akoma") ; 
-	System.out.println("This a asample output");
-	System.out.println("Kai kati akoma") ; 
-	
+
 }
 }
