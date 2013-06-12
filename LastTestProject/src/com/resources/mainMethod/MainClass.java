b724fe78c0de7de49272aa922762e1cdff205284
Galerakis Nikos <nikolisgal@gmail.com>
2013-06-12 15:09:00 +0200

    FirstCommit

---
+++ LastTestProject/src/com/resources/mainMethod/MainClass.java
@@ -0,0 +1,16 @@
+package com.resources.mainMethod;
+
+public class MainClass {
+
+	public static void main(String[] args){
+	System.out.println("This a asample output");
+	System.out.println("Kai kati akoma") ; 
+	System.out.println("This a asample output");
+	System.out.println("Kai kati akoma") ; 
+	System.out.println("This a asample output");
+	System.out.println("Kai kati akoma") ; 
+	System.out.println("This a asample output");
+	System.out.println("Kai kati akoma") ; 
+	
+}
+}
