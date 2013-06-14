edd4e720b26da76ea6affd61a7cd9437548ddfcd
iiuDevelopers <tsmgeor@gmail.com>
2013-06-14 11:53:21 +0300

    Update MainClass.java

--- LastTestProject/src/com/resources/mainMethod/MainClass.java
+++ LastTestProject/src/com/resources/mainMethod/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
 	public static void main(String[] args){
 	System.out.println("This a asample output");
 	System.out.println("Fucking Love it !") ; 
+	System.out.println("It Is Fucking Working") ; 
 }
 }
